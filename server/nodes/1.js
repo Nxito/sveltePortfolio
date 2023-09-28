f64f3074cdca42836596e26a0016649ecfc3a7fb
Nxito <55735074+Nxito@users.noreply.github.com>
2023-09-28 22:52:04 +0200

    Updates

--- server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.16f2b423.js","_app/immutable/chunks/scheduler.6f9dceae.js","_app/immutable/chunks/index.6032d4f1.js","_app/immutable/chunks/singletons.acedea9d.js","_app/immutable/chunks/paths.eb18cd76.js"];
-export const stylesheets = [];
-export const fonts = [];
--- server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.5d802d3d.js","_app/immutable/chunks/scheduler.6f9dceae.js","_app/immutable/chunks/index.6032d4f1.js","_app/immutable/chunks/icon.16591e5c.js","_app/immutable/chunks/paths.eb18cd76.js"];
-export const stylesheets = ["_app/immutable/assets/2.4515e959.css","_app/immutable/assets/icon.d96715fc.css"];
-export const fonts = [];
